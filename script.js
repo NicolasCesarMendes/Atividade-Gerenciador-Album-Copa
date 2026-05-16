518ba737e6656f9667b92c5fb3b2330a348f1eab
NicolasCesarMendes <nicolascesarmendes@gmail.com>
2026-05-16 11:38:56 -0300

    Implementação de CSS, Revisão do JavaScript e Adição de Comentários

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+// Função que calcula e retorna a porcentagem de progresso para completar o álbum.
 function calcularPorcentagem(total, coladas) {
-    let porcentagem = (coladas / total)*100;
-    return "Você já completou " + porcentagem + "% do álbum!";
+    let porcentagem = ((coladas / total) * 100).toFixed(2);
+    return porcentagem;
 }
 
+// Função que calcula e retorna quantas figurinhas faltam.
 function calcularFaltantes(total, coladas) {
     let faltantes = total - coladas;
-    return "Faltam " + faltantes + " figurinhas para completar o álbum.";
+    return faltantes;
 }
 
+// Função que gera a mensagem principal com base na porcentagem de progresso.
 function gerarStatusMensagem(porcentagem) {
     if (porcentagem == 100) {
         return "🚀 ÁLBUM COMPLETO! Você é um mestre colecionador!";
-    } else if (porcentagem >= 50) {
+    } else if (porcentagem >= 50 && porcentagem < 100) {
         return "💪 Falta pouco! Mais da metade já foi!";
-    } else {
+    } else if (porcentagem >= 0 && porcentagem < 50) {
         return "🌱 O álbum está no início, bora trocar!";
+    }
+    else {
+        return "Dados inválidos!";
     }
 }
 
+// Função principal que realiza a entrada, processamento e saída de dados.
 function atualizarPainel() {
     let total = Number(document.getElementById("total").value);
     let coladas = Number(document.getElementById("coladas").value);
@@ -31,9 +38,20 @@ function atualizarPainel() {
     let porcentagem = calcularPorcentagem(total, coladas);
     let faltantes = calcularFaltantes(total, coladas);
 
+    resultadoMensagem.innerHTML = gerarStatusMensagem(Number(porcentagem));
 
-    resultadoMensagem.innerHTML = gerarStatusMensagem(porcentagem);
-    resultadoProgresso.innerHTML = porcentagem;
-    resultadoFaltantes.innerHTML = faltantes;
-    resultadoRepetidas.innerHTML = repetidas;
+    resultadoProgresso.innerHTML = "Você já completou " + porcentagem + "% do álbum!";
+    if (resultadoMensagem.innerHTML == "Dados inválidos!") {
+        resultadoProgresso.innerHTML = "Dados não definidos! Por favor, digite os valores necessários.";
+    }
+
+    resultadoFaltantes.innerHTML = "Faltam " + faltantes + " figurinhas para completar o álbum.";
+    if (faltantes == 1) {
+        resultadoFaltantes.innerHTML =  "Falta " + faltantes + " figurinha para completar o álbum.";
+    }    
+
+    resultadoRepetidas.innerHTML = "Você tem " + repetidas + " figurinhas repetidas para trocar!";
+    if (repetidas == 1) {
+        resultadoRepetidas.innerHTML = "Você tem " + repetidas + " figurinha repetida para trocar!";
+    }
 }
